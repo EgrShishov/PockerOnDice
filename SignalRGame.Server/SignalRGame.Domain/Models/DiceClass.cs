408d3ec1864ed20806d59573873eccdcdaac1a1d
Sluma <bekarevstanislav@gmail.com>
2024-10-02 19:03:05 +0300

    add dice animation

--- SignalRGame.Server/SignalRGame.Domain/Models/DiceClass.cs
+++ SignalRGame.Server/SignalRGame.Domain/Models/DiceClass.cs
@@ -1,8 +1,9 @@
-﻿    namespace SignalRGame.Domain.Models;
+﻿namespace SignalRGame.Domain.Models;
 
 public class DiceClass
 {
     public int Id { get; set; }
     public int Value { get; set; }
-    public bool IsReroll { get; set; } = true; // если true, то выбран для следующего переброса
+    public bool IsReroll { get; set; } = true;
+    public bool IsRolling { get; set; } = false;// если true, то выбран для следующего переброса
 }
